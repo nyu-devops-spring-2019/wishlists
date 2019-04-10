822f145c5f29584002f475690e32a02036457c93
Nirmal Kagolanu <nk1970@stern.nyu.edu>
2019-04-10 18:17:16 -0400

    Added models file

--- app/models.py
+++ app/models.py
@@ -1,153 +1,297 @@
-# Copyright 2016, 2017 John Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""
-Models for Wishlist Demo Service
 
-All of the models are stored in this module
+ ######################################################################
+ # Copyright 2016, 2018 John Rofrano. All Rights Reserved.
+ #
+ # Licensed under the Apache License, Version 2.0 (the 'License');
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ # https://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an 'AS IS' BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ ######################################################################
+ """
+ Wishlists Model that uses Cloudant
 
-Models
-------
-Wishlist - A Wishlist used to save items
+ You must initlaize this class before use by calling inititlize().
+ This class looks for an environment variable called VCAP_SERVICES
+ to get it's database credentials from. If it cannot find one, it
+ tries to connect to Cloudant on the localhost. If that fails it looks
+ for a server name 'cloudant' to connect to.
 
-Attributes:
------------
-name (string) - the name of the wishlist
-customer_id (integer) - the id of the customer
-item_id (integer) - id of the item
+ To use with Docker couchdb database use:
+     docker run -d --name couchdb -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=pass couchdb
 
-"""
-import logging
-from flask_sqlalchemy import SQLAlchemy
+ Docker Note:
+     CouchDB uses /opt/couchdb/data to store its data, and is exposed as a volume
+     e.g., to use current folder add: -v $(pwd):/opt/couchdb/data
+     You can also use Docker volumes like this: -v couchdb_data:/opt/couchdb/data
+ """
 
-# Create the SQLAlchemy object to be initialized later in init_db()
-db = SQLAlchemy()
+ import os
+ import json
+ import logging
+ from retry import retry
+ from cloudant.client import Cloudant
+ from cloudant.query import Query
+ from requests import HTTPError, ConnectionError
 
-class DataValidationError(Exception):
-    """ Used for an data validation errors when deserializing """
-    pass
+ # get configruation from enviuronment (12-factor)
+ ADMIN_PARTY = os.environ.get('ADMIN_PARTY', 'False').lower() == 'true'
+ CLOUDANT_HOST = os.environ.get('CLOUDANT_HOST', 'localhost')
+ CLOUDANT_USERNAME = os.environ.get('CLOUDANT_USERNAME', 'admin')
+ CLOUDANT_PASSWORD = os.environ.get('CLOUDANT_PASSWORD', 'pass')
 
-class Wishlist(db.Model):
-    """
-    Class that represents a Wishlist
+ class DataValidationError(Exception):
+     """ Custom Exception with data validation fails """
+     pass
 
-    This version uses a relational database for persistence which is hidden
-    from us by SQLAlchemy's object relational mappings (ORM)
-    """
-    logger = logging.getLogger(__name__)
-    app = None
+ class Wishlist(object):
+     """ Wishlists interface to database """
 
-    # Table Schema
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
-    customer_id = db.Column(db.Integer)
-    item_id = db.Column(db.Integer)
+     logger = logging.getLogger(__name__)
+     client = None   # cloudant.client.Cloudant
+     database = None # cloudant.database.CloudantDatabase
 
-    def __repr__(self):
-        return '<Wishlist %r>' % (self.name)
+     def __init__(self, name=None, customer_id=None, item_id=None):
+         """ Constructor """
+         self.id = None
+         self.name = name
+         self.customer_id = customer_id
+         self.item_id = item_id
 
-    def save(self):
-        """
-        Saves a Wishlist to the data store
-        """
-        if not self.id:
-            db.session.add(self)
-        db.session.commit()
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def create(self):
+         """
+         Creates a new Wishlist in the database
+         """
+         if self.name is None:   # name is the only required field
+             raise DataValidationError('name attribute is not set')
 
-    def delete(self):
-        """ Removes a Wishlist from the data store """
-        db.session.delete(self)
-        db.session.commit()
+         try:
+             document = self.database.create_document(self.serialize())
+         except HTTPError as err:
+             Wishlist.logger.warning('Create failed: %s', err)
+             return
 
-    def serialize(self):
-        """ Serializes a Wishlistt into a dictionary """
-        return {"id": self.id,
-                "name": self.name,
-                "customer_id": self.customer_id,
-                "item_id": self.item_id}
+         if document.exists():
+             self.id = document['_id']
 
-    def deserialize(self, data):
-        """
-        Deserializes a Wishlist from a dictionary
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def update(self):
+         """
+         Updates a Wishlist in the database
+         """
+         try:
+             document = self.database[self.id]
+         except KeyError:
+             document = None
+         if document:
+             document.update(self.serialize())
+             document.save()
 
-        Args:
-            data (dict): A dictionary containing the Wishlist data
-        """
-        try:
-            self.name = data['name']
-            self.customer_id = data['customer_id']
-            self.item_id = data['item_id']
-        except KeyError as error:
-            raise DataValidationError('Invalid wishlist: missing ' + error.args[0])
-        except TypeError as error:
-            raise DataValidationError('Invalid wishlist: body of request contained' \
-                                      'bad or no data')
-        return self
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def save(self):
+         """ Saves a Wishlist in the database """
+         if self.name is None:   # name is the only required field
+             raise DataValidationError('name attribute is not set')
+         if self.id:
+             self.update()
+         else:
+             self.create()
 
-    @classmethod
-    def init_db(cls, app):
-        """ Initializes the database session """
-        cls.logger.info('Initializing database')
-        cls.app = app
-        # This is where we initialize SQLAlchemy from the Flask app
-        db.init_app(app)
-        app.app_context().push()
-        db.create_all()  # make our sqlalchemy tables
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def delete(self):
+         """ Deletes a Wishlist from the database """
+         try:
+             document = self.database[self.id]
+         except KeyError:
+             document = None
+         if document:
+             document.delete()
 
-    @classmethod
-    def all(cls):
-        """ Returns all of the Wishlists in the database """
-        cls.logger.info('Processing all Wishlists')
-        return cls.query.all()
+     def serialize(self):
+         """ serializes a Wishlist into a dictionary """
+         wishlist = {
+             "name": self.name,
+             "customer_id": self.customer_id,
+             "item_id": self.item_id
+         }
+         if self.id:
+             wishlist['_id'] = self.id
+         return wishlist
 
-    @classmethod
-    def find(cls, wishlist_id):
-        """ Finds a wishlist by it's ID """
-        cls.logger.info('Processing lookup for id %s ...', wishlist_id)
-        return cls.query.get(wishlist_id)
+     def deserialize(self, data):
+         """ deserializes a Wishlist my marshalling the data """
+         Wishlist.logger.info(data)
+         try:
+             self.name = data['name']
+             self.customer_id = data['customer_id']
+             self.item_id = data['item_id']
+         except KeyError as error:
+             raise DataValidationError('Invalid Wishlist: missing ' + error.args[0])
+         except TypeError as error:
+             raise DataValidationError('Invalid Wishlist: body of request contained bad or no data')
 
-    @classmethod
-    def find_or_404(cls, wishlist_id):
-        """ Find a wishlist by it's id """
-        cls.logger.info('Processing lookup or 404 for id %s ...', wishlist_id)
-        return cls.query.get_or_404(wishlist_id)
+         # if there is no id and the data has one, assign it
+         if not self.id and '_id' in data:
+             self.id = data['_id']
 
-    @classmethod
-    def find_by_name(cls, name):
-        """ Returns all wishlists with the given name
+         return self
 
-        Args:
-            name (string): the name of the wishlists you want to match
-        """
-        cls.logger.info('Processing name query for %s ...', name)
-        return cls.query.filter(cls.name == name)
 
-    @classmethod
-    def find_by_item_id(cls, item_id):
-        """ Returns all of the wishlists that contain the item
+ ######################################################################
+ #  S T A T I C   D A T A B S E   M E T H O D S
+ ######################################################################
 
-        Args:
-            item_id (integer): the item_id of the wishlists you want to match
-        """
-        cls.logger.info('Processing item_id query for %s ...', item_id)
-        return cls.query.filter(cls.item_id == item_id)
+     @classmethod
+     def connect(cls):
+         """ Connect to the server """
+         cls.client.connect()
 
-    @classmethod
-    def find_by_customer_id(cls, customer_id):
-        """ Returns all of the wishlists that contain the customer
+     @classmethod
+     def disconnect(cls):
+         """ Disconnect from the server """
+         cls.client.disconnect()
 
-        Args:
-            customer_id (integer): the customer_id of the wishlists you want to match
-        """
-        cls.logger.info('Processing customer_id query for %s ...', customer_id)
-        return cls.query.filter(cls.customer_id == customer_id)
+     @classmethod
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def create_query_index(cls, field_name, order='asc'):
+         """ Creates a new query index for searching """
+         cls.database.create_query_index(index_name=field_name, fields=[{field_name: order}])
+
+     @classmethod
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def remove_all(cls):
+         """ Removes all documents from the database (use for testing)  """
+         for document in cls.database:
+             document.delete()
+
+     @classmethod
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def all(cls):
+         """ Query that returns all Wishlists """
+         results = []
+         for doc in cls.database:
+             wishlist = Wishlist().deserialize(doc)
+             wishlist.id = doc['_id']
+             results.append(wishlist)
+         return results
+
+ ######################################################################
+ #  F I N D E R   M E T H O D S
+ ######################################################################
+
+     @classmethod
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def find_by(cls, **kwargs):
+         """ Find records using selector """
+         query = Query(cls.database, selector=kwargs)
+         results = []
+         for doc in query.result:
+             wishlist = Wishlist()
+             wishlist.deserialize(doc)
+             results.append(wishlist)
+         return results
+
+     @classmethod
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def find(cls, wishlist_id):
+         """ Query that finds Wishlists by their id """
+         try:
+             document = cls.database[wishlist_id]
+             return Wishlist().deserialize(document)
+         except KeyError:
+             return None
+
+     @classmethod
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def find_by_name(cls, name):
+         """ Query that finds Wishlists by their name """
+         return cls.find_by(name=name)
+
+     @classmethod
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def find_by_customer_id(cls, customer_id):
+         """ Query that finds Wishlists by their customer_id """
+         return cls.find_by(customer_id=customer_id)
+
+     @classmethod
+     @retry(HTTPError, delay=1, backoff=2, tries=5)
+     def find_by_item_id(cls, item_id):
+         """ Query that finds Items by their item_id """
+         return cls.find_by(item_id=item_id)
+
+ ############################################################
+ #  C L O U D A N T   D A T A B A S E   C O N N E C T I O N
+ ############################################################
+
+     @staticmethod
+     def init_db(dbname='wishlists'):
+         """
+         Initialized Coundant database connection
+         """
+         opts = {}
+         vcap_services = {}
+         # Try and get VCAP from the environment or a file if developing
+         if 'VCAP_SERVICES' in os.environ:
+             Wishlist.logger.info('Running in Bluemix mode.')
+             vcap_services = json.loads(os.environ['VCAP_SERVICES'])
+         # if VCAP_SERVICES isn't found, maybe we are running on Kubernetes?
+         elif 'BINDING_CLOUDANT' in os.environ:
+             Wishlist.logger.info('Found Kubernetes Bindings')
+             creds = json.loads(os.environ['BINDING_CLOUDANT'])
+             vcap_services = {"cloudantNoSQLDB": [{"credentials": creds}]}
+         else:
+             Wishlist.logger.info('VCAP_SERVICES and BINDING_CLOUDANT undefined.')
+             creds = {
+                 "username": CLOUDANT_USERNAME,
+                 "password": CLOUDANT_PASSWORD,
+                 "host": CLOUDANT_HOST,
+                 "port": 5984,
+                 "url": "http://"+CLOUDANT_HOST+":5984/"
+             }
+             vcap_services = {"cloudantNoSQLDB": [{"credentials": creds}]}
+
+         # Look for Cloudant in VCAP_SERVICES
+         for service in vcap_services:
+             if service.startswith('cloudantNoSQLDB'):
+                 cloudant_service = vcap_services[service][0]
+                 opts['username'] = cloudant_service['credentials']['username']
+                 opts['password'] = cloudant_service['credentials']['password']
+                 opts['host'] = cloudant_service['credentials']['host']
+                 opts['port'] = cloudant_service['credentials']['port']
+                 opts['url'] = cloudant_service['credentials']['url']
+
+         if any(k not in opts for k in ('host', 'username', 'password', 'port', 'url')):
+             Wishlist.logger.info('Error - Failed to retrieve options. ' \
+                              'Check that app is bound to a Cloudant service.')
+             exit(-1)
+
+         Wishlist.logger.info('Cloudant Endpoint: %s', opts['url'])
+         try:
+             if ADMIN_PARTY:
+                 Wishlist.logger.info('Running in Admin Party Mode...')
+             Wishlist.client = Cloudant(opts['username'],
+                                   opts['password'],
+                                   url=opts['url'],
+                                   connect=True,
+                                   auto_renew=True,
+                                   admin_party=ADMIN_PARTY
+                                  )
+         except ConnectionError:
+             raise AssertionError('Cloudant service could not be reached')
+
+         # Create database if it doesn't exist
+         try:
+             Wishlist.database = Wishlist.client[dbname]
+         except KeyError:
+             # Create a database using an initialized client
+             Wishlist.database = Wishlist.client.create_database(dbname)
+         # check for success
+         if not Wishlist.database.exists():
+             raise AssertionError('Database [{}] could not be obtained'.format(dbname))
