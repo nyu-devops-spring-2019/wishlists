64804d9a43339d1f56cb8cf15668b64cdb1bbe61
Xixin Liu <kay91401@gmail.com>
2019-04-15 20:07:55 -0400

    update init file

--- service/resources/__init__.py
+++ service/resources/__init__.py
@@ -4,4 +4,4 @@ This Package contains all opf the RESTful resources
 from .wishlist_resource import WishlistResource
 from .wishlist_collection import WishlistCollection
 from .home_page import HomePage
-from .purchase_action import PurchaseAction
+#from .purchase_action import PurchaseAction
